e56fc6924c25c43da96cfb2e8ebbdaf6ed03ff2d
Bogdan <BogdanG888G@yandex.ru>
2025-10-15 13:12:07 +0300

    Доработал парсер, пока накидал UI в Streamlit

---
+++ streamlit_ui.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Всем привет!')
+
+st.markdown('''
+            # Здарова
+            *Сейчас я буду прогонять эпохи! ;)*
+            ''')
+st.checkbox(label='Кликни на меня')
+
+st.balloons()
